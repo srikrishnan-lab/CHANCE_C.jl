4682bf67f23dfed104497fc06d36f25a5222294b
parinbhaduri <pbb62@cornell.edu>
2024-08-29 11:27:25 -0400

    Update timing script

--- test/time_profile.jl
+++ test/time_profile.jl
@@ -1,27 +1,45 @@
 
 ### For timing of model executions and specific evolution functions
+import Pkg
+Pkg.activate(dirname(@__DIR__))
+Pkg.instantiate()
+
+include(joinpath(dirname(@__DIR__), "src/CHANCE_C.jl"))
+using .CHANCE_C #add period since module is local to repository
+using CSV, DataFrames
+
+using BenchmarkTools
+
+## Load input Data
+balt_base = DataFrame(CSV.File(joinpath(dirname(pwd()), "baltimore-data/model_inputs/surge_area_baltimore_base.csv")))
+balt_levee = DataFrame(CSV.File(joinpath(dirname(pwd()), "baltimore-data/model_inputs/surge_area_baltimore_levee.csv")))
 
 
-using BenchmarkTools, TimerOutputs
-
-include("../src/model_initialization.jl")
-#Import model step function
-include("../src/model_evolution.jl")
-
-
-#Set input parameters
+#Define relevant parameters
 scenario = "Baseline"
 intervention = "Baseline"
 start_year = 2018
-no_of_years = 80
+no_of_years = 1
+perc_growth = 0.01
+perc_move = 0.025
+house_choice_mode = "flood_mem_utility"
+flood_coef = -10.0^5
+levee = false
+breach = true
+slr_scen = "high" #Select SLR Scenario to use (choice of "low", "medium", and "high")
+slr_rate = [3.03e-3,7.878e-3,2.3e-2] #Define SLR Rate of change for each scenario ( list order is "low", "medium", and "high")
+breach_null = 0.45 
+risk_averse = 0.3 
+flood_mem = 10 
+fixed_effect = 0
 
 
 ### Simple measure of model performance ###
 
-##Run once to compile functions (ignore output)
-balt_abm = Simulator(scenario = scenario, intervention = intervention, start_year = start_year, no_of_years = no_of_years)
-@time step!(balt_abm,dummystep,model_step!,no_of_years)
+##Performance Measure (Run 2x to account for compilation time)
+b = @benchmarkable step!(balt_abm, $dummystep, $CHANCE_C.model_step!, $no_of_years) setup=(balt_abm=Simulator(default_df, balt_base, balt_levee; slr_scen = slr_scen, slr_rate = slr_rate, scenario = scenario, intervention = intervention, start_year = start_year, no_of_years = no_of_years,
+pop_growth_perc = perc_growth, house_choice_mode = house_choice_mode, flood_coefficient = flood_coef, levee = false, breach = breach, breach_null = breach_null, risk_averse = risk_averse,
+ flood_mem = flood_mem, fixed_effect = fixed_effect)) seconds=10 evals=1
 
-##Actual performance measure
-balt_abm = Simulator(scenario = scenario, intervention = intervention, start_year = start_year, no_of_years = no_of_years)
-@time step!(balt_abm,dummystep,model_step!,no_of_years)
+run(b)
+

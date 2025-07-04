c1bc31fca889f9605c67ce7847a7ec32a6ee4391
parinbhaduri <pbb62@cornell.edu>
2025-07-04 13:33:00 -0400

    Fixed Flood Dynamics
    Fixed bug leading to misalignment between BG agents and their respective flood history. Also, standardized flood record instead of normalizing it

--- src/core/flood_dynamics.jl
+++ src/core/flood_dynamics.jl
@@ -142,7 +142,7 @@ end
 ## Input should be formatted as Dataframe with Rows representing BlockGroups and Columns representing Years 
     #(With first column being the BG GEOID column). 
 
-function flood_history(base_df; no_of_years = 10, start_year = 1981, slr_scen = "high", slr_rate = [3.03e-3,7.878e-3,2.3e-2])
+function flood_history(base_df; no_of_years = 10, start_year = 1981, slr_scen = "high", slr_rate = [3.03e-3,7.878e-3,2.3e-2], standardization = "min-max")
     #Sort df on GEOID
     sort!(base_df, :GEOID)
     #Select years of interest. Subset df
@@ -151,8 +151,12 @@ function flood_history(base_df; no_of_years = 10, start_year = 1981, slr_scen = 
     flood_mat = zeros(size(flood_df)[1], size(flood_df)[2], 1)
     flood_mat[:,:,1] = Matrix(flood_df)
     #Normalize flood extents
-    if std(flood_mat[:,:,1]) != 0
-        flood_mat[:,:,1] = (flood_mat[:,:,1] .- mean(flood_mat[:,:,1])) ./ std(flood_mat[:,:,1])
+    if standardization == "normal"
+        if std(flood_mat[:,:,1]) != 0
+            flood_mat[:,:,1] = (flood_mat[:,:,1] .- mean(flood_mat[:,:,1])) ./ std(flood_mat[:,:,1])
+        end
+    elseif standardization == "min-max"
+        flood_mat[:,:,1] = (flood_mat[:,:,1] .- minimum(flood_mat[:,:,1])) ./ (maximum(flood_mat[:,:,1]) .- minimum(flood_mat[:,:,1]))
     end
 
     scen_record = Int.(ones(no_of_years))
--- src/core/model_initialization.jl
+++ src/core/model_initialization.jl
@@ -19,15 +19,18 @@ mutable struct Properties{df<:Union{DataFrame, GroupedDataFrame{DataFrame}}, t_p
 end
 
 
-function Simulator(bg_df, pop_df, f_matrix, f_dict, model_evolve; 
-    no_of_years = 10, no_hhs_per_agent=10, simple_avoidance_perc = 0.95, house_budget_mode = "rhea", hh_budget_perc = 0.33, rhea_coef = 0.63, grouped = false, group_col = "adj_income_2019",
+function Simulator(bg_df, pop_df, f_df, model_evolve; 
+    start_year = 1981, no_of_years = 10, no_hhs_per_agent=10, simple_avoidance_perc = 0.95, house_budget_mode = "rhea", hh_budget_perc = 0.33, rhea_coef = 0.63, grouped = false, group_col = "adj_income_2019",
     cutoff_dict = OrderedDict(1=> [0,25000.00], 2=>[25000.00,75000.00], 3=>[75000.00, 1e7]), bg_cat = Dict(:col =>"income_cat", :group => [1,2,3]), pop_growth_perc = .01, 
     dist_param = [0.3, 0.4, 0.3], perc_move = 0.025, house_choice_mode = "simple_avoidance_utility", flood_coefficient = 500000, budget_reduction_perc = .90,
     simple_anova_coefficients = Dict(1=> [-121428, 294707, 130553, 128990, 154887, 72443], 2=> [-121428, 294707, 130553, 128990, 154887, 72443], 3=> [-121428, 294707, 130553, 128990, 154887, 72443]), 
     penalty = 50, stock_increase_mode = "simple_perc",  stock_increase_perc = .05,  housing_pricing_mode = "simple_perc", price_increase_perc = .05,
-    levee = false, risk_averse = 0.3, flood_mem = 10, fixed_effect = 0, seed = 1500,
+     standardization = "min-max", levee = false, risk_averse = 0.3, flood_mem = 10, fixed_effect = 0, seed = 1500,
 )
-
+    ##Calculate Flood matrix and Dict for ABM input
+    #Ensure that only BGs present in bg_df are selected in f_df
+    flood_bg_df = f_df[f_df.GEOID .∈ Ref(bg_df.GEOID),:]
+    f_matrix, f_dict = flood_history(flood_bg_df; no_of_years = no_of_years, start_year = start_year, standardization = standardization)
     
     ##Create Keyword Arguments for step function parameters
     #Flood Hazard & Flood Disamenity Updating
@@ -48,6 +51,7 @@ function Simulator(bg_df, pop_df, f_matrix, f_dict, model_evolve;
 
     #Set space for model 
     if grouped
+        sort!(bg_df,:GEOID)
         grouped_df = groupby(bg_df, :GEOID)
         width = Int(ceil(sqrt(size(grouped_df)[1])))
         space = GridSpace((width,width))
